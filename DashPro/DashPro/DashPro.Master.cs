3ca317ff43d8bb74134ec5a568431a234763876e
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2024-11-29 20:29:24 -0500

    añadir todos los cambios

---
+++ DashPro/DashPro/DashPro.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DashPro
+{
+    public partial class DashPro : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
