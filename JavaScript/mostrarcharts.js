273b57495abbb05aeddc2933621e1240e2ff90fb
silvvquiroz <silvana.quiroz@pucp.edu.pe>
2024-11-29 23:56:05 -0500

    arreglado

--- JavaScript/mostrarcharts.js
+++ JavaScript/mostrarcharts.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const barraropa = document.getElementById("barra-ropa");
     const barraalimentos = document.getElementById("barra-alimentos");
+    const barratransporte = document.getElementById("barra-transporte");
+    const barrasalidas = document.getElementById("barra-salidas");
+    const barraviajes = document.getElementById("barra-viajes");
+
     const chartropa = document.getElementById("linear-ropa");
     const chartalimentos = document.getElementById("linear-alimentos");
+    const charttransporte = document.getElementById("linear-transporte");
+    const chartsalidas = document.getElementById("linear-salidas");
+    const chartviajes = document.getElementById("linear-viajes");
 
     barraropa.addEventListener("click", function() {
         if (chartropa.classList.contains("d-none")) {
@@ -12,6 +19,18 @@ document.addEventListener("DOMContentLoaded", function() {
         if (chartalimentos.classList.contains("d-flex")) {
             chartalimentos.classList.add("d-none");
             chartalimentos.classList.remove("d-flex");
+        }
+        if (charttransporte.classList.contains("d-flex")) {
+            charttransporte.classList.add("d-none");
+            charttransporte.classList.remove("d-flex");
+        }
+        if (chartsalidas.classList.contains("d-flex")) {
+            chartsalidas.classList.add("d-none");
+            chartsalidas.classList.remove("d-flex");
+        }
+        if (chartviajes.classList.contains("d-flex")) {
+            chartviajes.classList.add("d-none");
+            chartviajes.classList.remove("d-flex");
         }
     });
 
@@ -24,6 +43,86 @@ document.addEventListener("DOMContentLoaded", function() {
             chartropa.classList.remove("d-flex");
             chartropa.classList.add("d-none");
         }
-        
+        if (charttransporte.classList.contains("d-flex")) {
+            charttransporte.classList.add("d-none");
+            charttransporte.classList.remove("d-flex");
+        }
+        if (chartsalidas.classList.contains("d-flex")) {
+            chartsalidas.classList.add("d-none");
+            chartsalidas.classList.remove("d-flex");
+        }
+        if (chartviajes.classList.contains("d-flex")) {
+            chartviajes.classList.add("d-none");
+            chartviajes.classList.remove("d-flex");
+        }
+    });
+
+    barratransporte.addEventListener("click", function() {
+        if (charttransporte.classList.contains("d-none")) {
+            charttransporte.classList.add("d-flex");
+            charttransporte.classList.remove("d-none");
+        }
+        if (chartropa.classList.contains("d-flex")) {
+            chartropa.classList.remove("d-flex");
+            chartropa.classList.add("d-none");
+        }
+        if (chartalimentos.classList.contains("d-flex")) {
+            chartalimentos.classList.add("d-none");
+            chartalimentos.classList.remove("d-flex");
+        }
+        if (chartsalidas.classList.contains("d-flex")) {
+            chartsalidas.classList.add("d-none");
+            chartsalidas.classList.remove("d-flex");
+        }
+        if (chartviajes.classList.contains("d-flex")) {
+            chartviajes.classList.add("d-none");
+            chartviajes.classList.remove("d-flex");
+        }
+    });
+
+    barrasalidas.addEventListener("click", function() {
+        if (chartsalidas.classList.contains("d-none")) {
+            chartsalidas.classList.add("d-flex");
+            chartsalidas.classList.remove("d-none");
+        }
+        if (chartropa.classList.contains("d-flex")) {
+            chartropa.classList.remove("d-flex");
+            chartropa.classList.add("d-none");
+        }
+        if (chartalimentos.classList.contains("d-flex")) {
+            chartalimentos.classList.add("d-none");
+            chartalimentos.classList.remove("d-flex");
+        }
+        if (charttransporte.classList.contains("d-flex")) {
+            charttransporte.classList.add("d-none");
+            charttransporte.classList.remove("d-flex");
+        }
+        if (chartviajes.classList.contains("d-flex")) {
+            chartviajes.classList.add("d-none");
+            chartviajes.classList.remove("d-flex");
+        }
+    });
+
+    barraviajes.addEventListener("click", function() {
+        if (chartviajes.classList.contains("d-none")) {
+            chartviajes.classList.add("d-flex");
+            chartviajes.classList.remove("d-none");
+        }
+        if (chartropa.classList.contains("d-flex")) {
+            chartropa.classList.remove("d-flex");
+            chartropa.classList.add("d-none");
+        }
+        if (chartalimentos.classList.contains("d-flex")) {
+            chartalimentos.classList.add("d-none");
+            chartalimentos.classList.remove("d-flex");
+        }
+        if (charttransporte.classList.contains("d-flex")) {
+            charttransporte.classList.add("d-none");
+            charttransporte.classList.remove("d-flex");
+        }
+        if (chartsalidas.classList.contains("d-flex")) {
+            chartsalidas.classList.add("d-none");
+            chartsalidas.classList.remove("d-flex");
+        }
     });
 });
